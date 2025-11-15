ac089464c4b3e1eb7c84d02053ae07a5e06a3c70
stefanBid <biddau.stefano99@gmail.com>
2025-11-15 20:36:30 +0100

    feat: enhance BaseButton and BaseCard components with improved class handling and default props

--- app/components/base/button/BaseButton.vue
+++ app/components/base/button/BaseButton.vue
@@ -1,23 +1,35 @@
 <script setup lang="ts">
 interface BaseButtonProps {
   variant?: 'primary' | 'secondary' | 'outline'
+  type?: 'button' | 'submit' | 'reset'
+  isDisabled?: boolean
+  isLoading?: boolean
 }
 
 // Input / Output
 const props = withDefaults(defineProps<BaseButtonProps>(), {
   variant: 'primary',
+  type: 'button',
+  isDisabled: false,
+  isLoading: false,
 })
 </script>
 
 <template>
   <button
-    class="ty-sb-btn-label border px-4 py-2 sm:px-6 sm:py-3 rounded-xl u-sb-soft-transition cursor-pointer"
+    class="ty-sb-btn-label border px-4 py-2 sm:px-6 sm:py-3 rounded-xl u-sb-soft-transition inline-flex items-center u-sb-focus"
     :class="{
+      'cursor-pointer': !props.isDisabled && !props.isLoading,
+      'opacity-45 cursor-not-allowed': props.isDisabled || props.isLoading,
       'bg-sb-accent hover:bg-sb-accent-hover border-sb-accent-border text-sb-contrast': props.variant === 'primary',
       'bg-sb-surface-2 hover:bg-sb-surface border-sb-border  text-sb-contrast': props.variant === 'secondary',
       'bg-transparent border-sb-accent text-sb-accent hover:bg-sb-accent hover:text-sb-main': props.variant=== 'outline',
     }"
+
+    :disabled="props.isDisabled || props.isLoading"
+    :type="props.type"
   >
     <slot></slot>
+    <Icon v-if="props.isLoading" class="inline-block size-5 sm:size-6 ml-2 animate-spin" name="solar:refresh-circle-broken" />
   </button>
 </template>
--- app/components/base/card/BaseCard.vue
+++ app/components/base/card/BaseCard.vue
@@ -20,9 +20,9 @@ const props = withDefaults(defineProps<BaseCardProps>(), {
     class="flex flex-col p-4 sm:p-6 rounded-xl border border-sb-border shadow-[0_4px_20px_var(--color-sb-shadow)] u-sb-soft-transition"
     :class="{
       'bg-sb-surface': props.variant === 'dark',
-      'bg-sb-surface hover:bg-sb-surface-2': props.variant === 'dark-hover',
+      'bg-sb-surface hover:bg-sb-surface-2 focus-within:bg-sb-surface-2': props.variant === 'dark-hover',
       'bg-sb-surface-2': props.variant === 'light',
-      'bg-sb-surface-2 hover:bg-sb-surface': props.variant === 'light-hover',
+      'bg-sb-surface-2 hover:bg-sb-surface focus-within:bg-sb-surface   ': props.variant === 'light-hover',
     }"
   >
     <template v-if="props.fullCustomContent">
