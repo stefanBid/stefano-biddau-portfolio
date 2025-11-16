0cd9fcaa80bfd6c95ca25f96f76f926a33e870ed
stefanBid <biddau.stefano99@gmail.com>
2025-11-16 17:03:39 +0100

    feat: add BaseInput component with props for label, placeholder, and type

---
+++ app/components/base/input/BaseInput.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+interface BaseInputProps {
+  label?: string
+  placeholder?: string
+  type?: 'text' | 'password' | 'email' | 'number'
+}
+// Input / Output
+const props = withDefaults(defineProps<BaseInputProps>(), {
+  label: undefined,
+  placeholder: 'Insert a value...',
+  type: 'text',
+})
+
+const model = defineModel<string>('input')
+</script>
+
+<template>
+  <div class="space-y-3">
+    <label class="ty-sb-label block text-sb-muted">{{ props.label }}</label>
+    <input
+      v-model="model"
+      class="w-full rounded-lg bg-sb-surface-2 border border-sb-border px-4 py-2 text-sb-contrast ty-sb-paragraph focus:outline-none focus:ring-2 focus:ring-sb-accent"
+      :placeholder="props.placeholder"
+      :type="props.type"
+    />
+  </div>
+</template>
