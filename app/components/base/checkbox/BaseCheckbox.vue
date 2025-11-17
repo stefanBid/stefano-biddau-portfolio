954a7c6ff12f410d20c119396fad89566433e0e5
stefanBid <biddau.stefano99@gmail.com>
2025-11-17 10:33:08 +0100

    feat: update BaseCheckbox component to support rich text labels and enhance styling

--- app/components/base/checkbox/BaseCheckbox.vue
+++ app/components/base/checkbox/BaseCheckbox.vue
@@ -3,6 +3,7 @@ interface BaseCheckboxProps {
   id: string
   name?: string
   label: string
+  isRich?: boolean
   invalid?: boolean
 }
 
@@ -10,28 +11,56 @@ interface BaseCheckboxProps {
 const props = withDefaults(defineProps<BaseCheckboxProps>(), {
   name: undefined,
   invalid: false,
+  isRich: false,
 })
 
 const model = defineModel<boolean>('input')
 </script>
 
 <template>
-  <div
-    class="inline-flex h-fit items-center gap-2"
-  >
+  <div class="inline-flex h-fit items-center gap-2">
     <input
       :id="props.id"
       v-model="model"
       :aria-invalid="props.invalid ? 'true' : 'false'"
       autocomplete="on"
-      class="size-4 rounded border bg-sb-surface-2 text-sb-accent focus:ring-2 focus:ring-sb-accent u-sb-soft-transition"
+      class="
+      relative
+      peer size-4.5 sm:size-5 shrink-0 rounded-md border
+      u-sb-soft-transition cursor-pointer
+      appearance-none
+      outline-none
+      bg-sb-surface-2
+      border-sb-border
+      checked:bg-sb-accent-hover
+      checked:border-sb-contrast
+      focus-visible:ring-2
+      focus-visible:ring-sb-accent
+      before:absolute
+      before:left-1/2 before:top-1/2
+      before:-translate-x-1/2 before:-translate-y-1/2
+      before:flex before:items-center before:justify-center
+      before:text-sb-contrast before:text-lg
+      before:content-['✓'] before:opacity-0
+      checked:before:opacity-100
+    "
       :class="props.invalid ? 'border-sb-danger' : 'border-sb-border'"
       :name="props.name || `${props.id}-name`"
       type="checkbox"
     />
 
-    <span class="ty-sb-paragraph text-sb-contrast u-sb-soft-transition">
+    <span
+      v-if="props.isRich"
+      class="ty-sb-caption text-sb-contrast u-sb-soft-transition"
+    >
       {{ props.label }}
+    </span>
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <span
+      v-else
+      class="ty-sb-caption text-sb-contrast u-sb-soft-transition"
+      v-html="props.label"
+    >
     </span>
   </div>
 </template>
