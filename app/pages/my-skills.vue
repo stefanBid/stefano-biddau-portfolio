09f1ece7953ac95a6b3d9b618e4bb96f16456353
stefanBid <biddau.stefano99@gmail.com>
2025-11-27 09:47:19 +0100

    feat: add page initialization

---
+++ app/pages/my-skills.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+// Dependencies
+const { t } = useI18n()
+const route = useRoute()
+
+useSeoMeta({
+  // LOCALIZED
+  title: () => t('meta.skills.title'),
+  description: () => t('meta.skills.description'),
+  ogTitle: () => t('meta.skills.ogTitle'),
+  ogDescription: () => t('meta.skills.description'),
+  twitterTitle: () => t('meta.skills.ogTitle'),
+  twitterDescription: () => t('meta.skills.description'),
+
+  // GLOBALS
+  ogImage: '/images/card-logo.jpg',
+  twitterImage: '/images/card-logo.jpg',
+  twitterCard: 'summary',
+
+  // DYNAMIC BUT NOT TIED TO CONTENT LANGUAGE
+  ogUrl: () => `https://www.stefanobiddau.com${route.fullPath}`,
+})
+
+// State
+
+// Events
+</script>
+
+<template>
+  <div>
+    <ThePageHero
+      id="my-skills"
+      :text="t('pages.skills.hero')"
+    />
+  </div>
+</template>
