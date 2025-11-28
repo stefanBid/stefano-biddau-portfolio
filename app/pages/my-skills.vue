5ee5b44d793263f61df9e90ff7b730b705945599
stefanBid <biddau.stefano99@gmail.com>
2025-11-28 13:56:15 +0100

    feat: update skills section with dynamic content and solar system animation

--- app/pages/my-skills.vue
+++ app/pages/my-skills.vue
@@ -35,29 +35,90 @@ const frontendIcons = [
       :text="t('pages.skills.hero')"
     />
     <section aria-label="Skills and Technologies" class=" w-full py-20 flex  flex-col gap-20">
+      <!-- CARD FRONTEND SKILLS -->
       <BaseCard
-        v-for="value in 3"
-        :key="value"
         class="p-0!"
         full-custom-content
         variant="dark"
       >
-        <div class="h-[450px] relative  flex items-center overflow-hidden">
+        <div class="h-[470px] relative flex items-center overflow-hidden">
           <!-- Contenuto a sinistra -->
-          <div class="flex flex-col h-full justify-center z-10 p-4 md:p-6 bg-sb-surface/60 rounded-xl u-sb-soft-transition">
+          <div class="flex flex-col w-full lg:w-2/3 h-full justify-center z-10 p-4 md:p-6 bg-sb-surface/70 rounded-xl u-sb-soft-transition">
             <h2
               class="ty-sb-title u-sb-soft-transition"
             >
-              Skill Category {{ value }}
+              {{ t('pages.skills.frontendCard.title') }}
             </h2>
             <p
               class="ty-sb-paragraph text-justify mt-3 md:mt-4 u-sb-soft-transition"
             >
-              Descrizione della skill e delle tecnologie usate.
+              {{ t('pages.skills.frontendCard.paragraph') }}
             </p>
-            <div>
+            <div class="mt-6 md:mt-8 u-sb-soft-transition">
               <BaseButton variant="primary">
-                Scopri di più
+                {{ t('pages.skills.frontendCard.buttonText') }}
+              </BaseButton>
+            </div>
+          </div>
+          <!-- Sistema solare a destra, metà visibile -->
+          <div class="absolute top-1/2 right-0 -translate-y-1/2 translate-x-1/2 w-[300px] h-[300px] flex items-center justify-end pointer-events-none ">
+            <CustomSolarSystem :planets-icon="frontendIcons" />
+          </div>
+        </div>
+      </BaseCard>
+      <!-- CARD BACKEND SKILLS -->
+      <BaseCard
+        class="p-0!"
+        full-custom-content
+        variant="dark"
+      >
+        <div class="h-[470px] relative flex items-center overflow-hidden">
+          <!-- Contenuto a sinistra -->
+          <div class="flex flex-col w-full lg:w-2/3 h-full justify-center z-10 p-4 md:p-6 bg-sb-surface/70 rounded-xl u-sb-soft-transition">
+            <h2
+              class="ty-sb-title u-sb-soft-transition"
+            >
+              {{ t('pages.skills.backendCard.title') }}
+            </h2>
+            <p
+              class="ty-sb-paragraph text-justify mt-3 md:mt-4 u-sb-soft-transition"
+            >
+              {{ t('pages.skills.backendCard.paragraph') }}
+            </p>
+            <div class="mt-6 md:mt-8 u-sb-soft-transition">
+              <BaseButton variant="primary">
+                {{ t('pages.skills.backendCard.buttonText') }}
+              </BaseButton>
+            </div>
+          </div>
+          <!-- Sistema solare a destra, metà visibile -->
+          <div class="absolute top-1/2 right-0 -translate-y-1/2 translate-x-1/2 w-[300px] h-[300px] flex items-center justify-end pointer-events-none ">
+            <CustomSolarSystem :planets-icon="frontendIcons" />
+          </div>
+        </div>
+      </BaseCard>
+      <!-- CARD DESIGN SKILLS -->
+      <BaseCard
+        class="p-0!"
+        full-custom-content
+        variant="dark"
+      >
+        <div class="h-[470px] relative flex items-center overflow-hidden">
+          <!-- Contenuto a sinistra -->
+          <div class="flex flex-col w-full lg:w-2/3 h-full justify-center z-10 p-4 md:p-6 bg-sb-surface/70 rounded-xl u-sb-soft-transition">
+            <h2
+              class="ty-sb-title u-sb-soft-transition"
+            >
+              {{ t('pages.skills.designCard.title') }}
+            </h2>
+            <p
+              class="ty-sb-paragraph text-justify mt-3 md:mt-4 u-sb-soft-transition"
+            >
+              {{ t('pages.skills.designCard.paragraph') }}
+            </p>
+            <div class="mt-6 md:mt-8 u-sb-soft-transition">
+              <BaseButton variant="primary">
+                {{ t('pages.skills.designCard.buttonText') }}
               </BaseButton>
             </div>
           </div>
