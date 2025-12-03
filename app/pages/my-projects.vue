e05e09fe93edee12191aed407c874bf802dd939f
stefanBid <biddau.stefano99@gmail.com>
2025-12-03 21:18:50 +0100

    feat: replace NuxtImg with standard img tag for SBT logo on project page

--- app/pages/my-projects.vue
+++ app/pages/my-projects.vue
@@ -135,7 +135,7 @@ watch(fetchError, (newError) => {
         <div class="flex flex-col gap-3 md:gap-4 u-sb-soft-transition">
           <div class="flex items-center justify-center mb-4 md:mb-6 ">
             <div class="w-fit bg-sb-contrast rounded-xl p-4 md:p-6 shadow-[0_8px_30px_var(--color-sb-shadow)] u-sb-soft-transition">
-              <NuxtImg
+              <img
                 alt="SBT Logo"
                 class="w-25 md:w-40 u-sb-soft-transition"
                 src="/images/sbt-logo.png"
